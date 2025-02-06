4999c8292c980d8a4651b7d5cc3e5938cacb12fe
WeirdGub <radioactivepotato77@gmail.com>
2025-02-06 16:01:36 -0500

    Added test code for analog and digital input pins.

--- main.cpp
+++ main.cpp
@@ -13,42 +13,36 @@
 
 // Front wheel drive, front wheels are normal, back wheels are omni-wheels
 // Tank drive
-FEHMotor FLWheelMotor0 (FEHMotor::Motor0, 5.0);
-FEHMotor FRWheelMotor1 (FEHMotor::Motor1, 5.0);
+FEHMotor FLWheelMotor0 (FEHMotor::Motor0, 9.0);
+FEHMotor FRWheelMotor1 (FEHMotor::Motor1, 9.0);
+//declares an analog input pin on P0_3
+AnalogInputPin CdS_cell(FEHIO::P0_3);
+//declares a digital input pin on P2_1
+DigitalInputPin bump_switch(FEHIO::P2_1);
 
 int main(void)
 {
-    double time = TimeNow();
-    double elapsedTime = 0;
+    //read the value of the CdS_cell and store in x
+    float cdsValue = CdS_cell.Value();
+    //read the value of the digital input into x
+    bool switchy = bump_switch.Value();
     LCD.Clear(BLACK);
     LCD.WriteLine("Testing Front Left Forward");
     FLWheelMotor0.SetPercent(100.);
-    while (time < 1.)
-    {
-        time = TimeNow();
-    }
+    Sleep(1.);
     LCD.Clear(BLACK);
     LCD.WriteLine("Testing Front Left Reverse");
     FLWheelMotor0.SetPercent(-100.);
-    while (time < 2.)
-    {
-        time = TimeNow();
-    }
-    FLWheelMotor0.SetPercent(0.);
+    Sleep(1.);
+    FLWheelMotor0.Stop();
     LCD.Clear(BLACK);
     LCD.WriteLine("Testing Front Right Forward");
     FRWheelMotor1.SetPercent(100.);
-    while (time < 3.)
-    {
-        time = TimeNow();
-    }
+    Sleep(1.);
     LCD.Clear(BLACK);
     LCD.WriteLine("Testing Front Right Reverse");
     FRWheelMotor1.SetPercent(-100.);
-    while (time < 4.)
-    {
-        time = TimeNow();
-    }
-    FRWheelMotor1.SetPercent(0.);
+    Sleep(1.);
+    FRWheelMotor1.Stop();
 	return 0;
 }
