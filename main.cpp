de5e72c83ce5459a220635a5abec90e0eced7c66
Feltlin <82766584+Feltlin@users.noreply.github.com>
2025-02-24 10:46:42 -0500

    Initial PID method.

--- main.cpp
+++ main.cpp
@@ -8,10 +8,9 @@
 #include <FEHBuzzerNoSleep.h>
 #include <FEHRCS.h>
 #include <FEHSD.h>
-#include <string.h>
-#include <stdio.h>
-#include <time.h>
-#include <thread>
+#include <string>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 void colorSensor(){
     // Front wheel drive, front wheels are normal, back wheels are omni-wheels
@@ -210,170 +209,26 @@ struct Note{
 
 enum{
     C8  = 4186,
-    B7  = 3951,
-    As7 = 3729,
-    Bf7 = 3729,
-    A7  = 3520,
-    Gs7 = 3322,
-    Af7 = 3322,
-    G7  = 3136,
-    Fs7 = 2960,
-    Gf7 = 2960,
-    F7  = 2794,
-    E7  = 2637,
-    Ds7 = 2489,
-    Ef7 = 2489,
-    D7  = 2349,
-    Cs7 = 2217,
-    Df7 = 2217,
-    C7  = 2093,
-    B6  = 1976,
-    As6 = 1865,
-    Bf6 = 1865,
-    A6  = 1760,
-    Gs6 = 1661,
-    Af6 = 1661,
-    G6  = 1568,
-    Fs6 = 1480,
-    Gf6 = 1480,
-    F6  = 1397,
-    E6  = 1319,
-    Ds6 = 1245,
-    Ef6 = 1244,
-    D6  = 1175,
-    Cs6 = 1109,
-    Df6 = 1109,
-    C6  = 1047,
-    B5  = 988,
-    As5 = 932,
-    Bf5 = 932,
-    A5  = 880,
-    Gs5 = 831,
-    Af5 = 831,
-    G5  = 784,
-    Fs5 = 740,
-    Gf5 = 740,
-    F5  = 698,
-    E5  = 659,
-    Ds5 = 622,
-    Ef5 = 622,
-    D5  = 587,
-    Cs5 = 554,
-    Df5 = 554,
-    C5  = 523,
-    B4  = 494,
-    As4 = 466,
-    Bf4 = 466,
-    A4  = 440,
-    Gs4 = 415,
-    Af4 = 415,
-    G4  = 392,
-    Fs4 = 370,
-    Gf4 = 370,
-    F4  = 349,
-    E4  = 330,
-    Ds4 = 311,
-    Ef4 = 311,
-    D4  = 294,
-    Cs4 = 277,
-    Df4 = 277,
-    C4  = 261,
-    B3  = 247,
-    As3 = 233,
-    Bf3 = 233,
-    A3  = 220,
-    Gs3 = 208,
-    Af3 = 208,
-    G3  = 196,
-    Fs3 = 185,
-    Gf3 = 185,
-    F3  = 175,
-    E3  = 165,
-    Ds3 = 156,
-    Ef3 = 156,
-    D3  = 147,
-    Cs3 = 139,
-    Df3 = 139,
-    C3  = 131,
-    B2  = 123,
-    As2 = 117,
-    Bf2 = 117,
-    A2  = 110,
-    Gs2 = 104,
-    Af2 = 104,
-    G2  = 98,
-    Fs2 = 92,
-    Gf2 = 92,
-    F2  = 87,
-    E2  = 82,
-    Ds2 = 78,
-    Ef2 = 78,
-    D2  = 73,
-    Cs2 = 69,
-    Df2 = 69,
-    C2  = 65,
-    B1  = 62,
-    As1 = 58,
-    Bf1 = 58,
-    A1  = 55,
-    Gs1 = 52,
-    Af1 = 52,
-    G1  = 49,
-    Fs1 = 46,
-    Gf1 = 46,
-    F1  = 44,
-    E1  = 41,
-    Ds1 = 39,
-    Ef1 = 39,
-    D1  = 37,
-    Cs1 = 35,
-    Df1 = 35,
-    C1  = 33,
-    B0  = 31,
-    As0 = 29,
-    Bf0 = 29,
-    A0  = 28
+    B7  = 3951, As7 = 3729, Bf7 = 3729, A7  = 3520, Gs7 = 3322, Af7 = 3322, G7  = 3136, Fs7 = 2960, Gf7 = 2960, F7  = 2794, E7  = 2637, Ds7 = 2489, Ef7 = 2489, D7  = 2349, Cs7 = 2217, Df7 = 2217, C7  = 2093,
+    B6  = 1976, As6 = 1865, Bf6 = 1865, A6  = 1760, Gs6 = 1661, Af6 = 1661, G6  = 1568, Fs6 = 1480, Gf6 = 1480, F6  = 1397, E6  = 1319, Ds6 = 1245, Ef6 = 1244, D6  = 1175, Cs6 = 1109, Df6 = 1109, C6  = 1047,
+    B5  = 988, As5 = 932, Bf5 = 932, A5  = 880, Gs5 = 831, Af5 = 831, G5  = 784, Fs5 = 740, Gf5 = 740, F5  = 698, E5  = 659, Ds5 = 622, Ef5 = 622, D5  = 587, Cs5 = 554, Df5 = 554, C5  = 523,
+    B4  = 494, As4 = 466, Bf4 = 466, A4  = 440, Gs4 = 415, Af4 = 415, G4  = 392, Fs4 = 370, Gf4 = 370, F4  = 349, E4  = 330, Ds4 = 311, Ef4 = 311, D4  = 294, Cs4 = 277, Df4 = 277, C4  = 261,
+    B3  = 247, As3 = 233, Bf3 = 233, A3  = 220, Gs3 = 208, Af3 = 208, G3  = 196, Fs3 = 185, Gf3 = 185, F3  = 175, E3  = 165, Ds3 = 156, Ef3 = 156, D3  = 147, Cs3 = 139, Df3 = 139, C3  = 131,
+    B2  = 123, As2 = 117, Bf2 = 117, A2  = 110, Gs2 = 104, Af2 = 104, G2  = 98, Fs2 = 92, Gf2 = 92, F2  = 87, E2  = 82, Ds2 = 78, Ef2 = 78, D2  = 73, Cs2 = 69, Df2 = 69, C2  = 65,
+    B1  = 62, As1 = 58, Bf1 = 58, A1  = 55, Gs1 = 52, Af1 = 52, G1  = 49, Fs1 = 46, Gf1 = 46, F1  = 44, E1  = 41, Ds1 = 39, Ef1 = 39, D1  = 37, Cs1 = 35, Df1 = 35, C1  = 33,
+    B0  = 31, As0 = 29, Bf0 = 29, A0  = 28
 };
 
-int main(){
+void asyncMusic(){
     Note song[] = {
-        {500, E4},
-        {500, E4},
-        {500, F4},
-        {500, G4},
-
-        {500, G4},
-        {500, F4},
-        {500, E4},
-        {500, D4},
-
-        {500, C4},
-        {500, C4},
-        {500, D4},
-        {500, E4},
-
-        {750, E4},
-        {250, D4},
-        {1000, D4},
-
-        {500, E4},
-        {500, E4},
-        {500, F4},
-        {500, G4},
-
-        {500, G4},
-        {500, F4},
-        {500, E4},
-        {500, D4},
-
-        {500, C4},
-        {500, C4},
-        {500, D4},
-        {500, E4},
-
-        {750, D4},
-        {250, C4},
-        {1000, C4},
+        {500, E4}, {500, E4}, {500, F4}, {500, G4},
+        {500, G4}, {500, F4}, {500, E4}, {500, D4},
+        {500, C4}, {500, C4}, {500, D4}, {500, E4},
+        {750, E4}, {250, D4}, {1000, D4},
+        {500, E4}, {500, E4}, {500, F4}, {500, G4},
+        {500, G4}, {500, F4}, {500, E4}, {500, D4},
+        {500, C4}, {500, C4}, {500, D4}, {500, E4},
+        {750, D4}, {250, C4}, {1000, C4},
     };
     unsigned long t0 = TimeNowMSec(), t1 = t0, ta = 0;
     int i = 0;
@@ -393,5 +248,121 @@ int main(){
         }
         LCD.WriteLine(int(t1));
     }
+}
+
+DigitalEncoder encoder[3] = {
+    DigitalEncoder(FEHIO::P0_0),
+    DigitalEncoder(FEHIO::P0_1),
+    DigitalEncoder(FEHIO::P0_2),
+};
+FEHMotor motor[3] = {
+    FEHMotor(FEHMotor::Motor0, 9.0),
+    FEHMotor(FEHMotor::Motor1, 9.0),
+    FEHMotor(FEHMotor::Motor2, 9.0),
+};
+
+class State{
+    public:
+        double inchPerCount = 2.5 * M_PI / 318;
+
+        //{Front, Left, Right}
+        double newError[3] = {0, 0, 0}, lastError[3] = {0, 0, 0}, errorSum[3] = {0, 0, 0}, errorDel[3] = {0, 0, 0};
+        double PTerm[3] = {0, 0, 0}, ITerm[3] = {0, 0, 0}, DTerm[3] = {0, 0, 0};
+        double PConst[3] = {0.03, 0.03, 0.03}, IConst[3] = {0, 0, 0}, DConst[3] = {1e-3, 1e-3, 1e-3};
+
+        double actualSpeed[3] = {0, 0, 0}, expectedSpeed[3] = {0, 0, 0};
+        double power[3] = {0, 0, 0};
+        double distance[3] = {0, 0, 0};
+        int direction[3] = {1, 1, 1};
+
+        int newCount[3] = {0, 0, 0}, lastCount[3] = {0, 0, 0};
+
+        unsigned long newTime = 0, lastTime = 0;
+
+        std::string state = "start";
+        bool init = true;
+
+    State(){
+        for(int i = 0; i < 3; ++i){
+            encoder[i].ResetCounts();
+        }
+    }
+
+    void updateTime(){
+        lastTime = newTime;
+        newTime = TimeNowMSec();
+    }
+
+    void updateState(){
+        if(state == "start"){
+            start(); 
+        }
+    }
+
+    void start(){
+        if(init){
+            init = false;
+            expectedSpeed[0] = 0;
+            expectedSpeed[1] = 2;
+            expectedSpeed[2] = 2;
+            direction[1] = 1;
+            direction[2] = -1;
+            power[1] = 20;
+            power[2] = 20;
+            motor[1].SetPercent(power[1] * direction[1]);
+            motor[2].SetPercent(power[2] * direction[2]);
+
+        }
+        else if ((newCount[1] + newCount[2]) / 2 * inchPerCount < 200){
+            Sleep(1);
+            for(int i = 0; i < 3; ++i){
+                if(i == 1){
+                    LCD.Write(" Left: ");
+                    LCD.Write(power[i]);
+                }
+                else if(i == 2){
+                    LCD.Write(" Right: ");
+                    LCD.Write(power[i]);
+                }
+                
+                lastCount[i] = newCount[i];
+                newCount[i] = encoder[i].Counts();
+                actualSpeed[i] = inchPerCount * (newCount[i] - lastCount[i]) / (newTime - lastTime) * 1000;
+                lastError[i] = newError[i];
+                newError[i] = expectedSpeed[i] - actualSpeed[i];
+                errorDel[i] = (newError[i] - lastError[i]) / (newTime - lastTime) * 1000;
+                errorSum[i] += newError[i];
+                PTerm[i] = newError[i] * PConst[i];
+                ITerm[i] = errorSum[i] * IConst[i];
+                DTerm[i] = errorDel[i] * DConst[i];
+                power[i] = power[i] + PTerm[i] + ITerm[i] + DTerm[i];
+                motor[i].SetPercent(power[i] * direction[i]);
+            }
+        }
+        else{
+            for(int i = 0; i < 3; ++i){
+                motor[i].Stop();
+            }
+        }
+    }
+};
+
+State state;
+
+int main(){
+    while(true){
+        state.updateTime();
+        state.updateState();
+    }
+    // FEHMotor frontMotor = FEHMotor(FEHMotor::Motor0, 9.0);
+    // DigitalEncoder frontEncoder = DigitalEncoder(FEHIO::P0_0);
+
+    // frontMotor.SetPercent(20.);
+
+    // while(frontEncoder.Counts() <= 318 * 5){
+    //     LCD.WriteLine(frontEncoder.Counts());
+    // }
+    // frontMotor.Stop();
+
 	return 0;
 }
